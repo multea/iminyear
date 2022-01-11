59e92bc3c4f55c917821790099f1ced5b069db8a
multea.dev <multea.dev@gmail.com>
2022-01-11 22:00:37 +0900

    2022-01-11

---
+++ C++/HelloWorld/HelloWorld/Main.cpp
@@ -0,0 +1,15 @@
+#include <cstdio> //전처리.
+
+// 프로그램의 진입점, 모든 프로그램은 이 main 을 가지고 있어야 함.
+int main() {
+	
+	int apple = 10;
+	int banana = 20;
+	int result;
+
+	result = apple + banana;
+
+	printf("%d",result);
+
+	return 0;
+}
