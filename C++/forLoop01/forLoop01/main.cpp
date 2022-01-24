7287b1a31cf91474ef81ba70c8ef3db736bd64f6
multea.dev <multea.dev@gmail.com>
2022-01-24 21:36:37 +0900

    2022-01-24

--- C++/forLoop01/forLoop01/main.cpp
+++ C++/forLoop01/forLoop01/main.cpp
@@ -8,8 +8,16 @@ int main() {
 	fseek(stdin,0,SEEK_END);
 	scanf_s("%d", &input);
 
-	
-	printf("Result : %d" ,input);
+	for (int i = 0; i < input; i++) {
+		for (int k = 0; k < i ; k++) {
+			printf(" ");
+		}
+		for (int j = 0; j < input - i; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+
 
 	return 0;
 }
